e632651a156e7dfbfc0f121a535d9dc172b37b2e
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-04-29 09:53:44 +0530

    added Difficulty Level

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -18,12 +19,61 @@ namespace Sha256HashCode {
                 return builder.ToString ();
             }
         }
-        static void Main (string[] args) {
+
+        public static void GenerateHashcodeWithoutDataChange () {
             string rawData = "Hello World!";
             for (int i = 1; i <= 10; i++) {
                 string hash = CalculateHash (rawData);
-                Console.WriteLine (hash);
+                Console.WriteLine (hash + " input Data " + rawData);
             }
+        }
+
+        public static void GenerateHashcodeWithDataChange () {
+            for (int i = 1; i <= 10; i++) {
+                string rawData = "Hello World!" + i.ToString ();
+                string hash = CalculateHash (rawData);
+                Console.WriteLine (hash + " input Data " + rawData);
+            }
+        }
+
+        public static void GenerateHashcodeWithDifficultyLevel (int difficultyLevel) {
+            string rawData = "Hello World!";
+            int numberOfInterations = 0;
+            string hashValidationTemplate = new String ('0', difficultyLevel);
+            rawData = "Hello World!" + numberOfInterations.ToString ();
+            string hash = CalculateHash (rawData);
+            while (hash.Substring (0, difficultyLevel) != hashValidationTemplate) {
+                //// Console.WriteLine (hash);
+                rawData = "Hello World!" + numberOfInterations.ToString ();
+                hash = CalculateHash (rawData);
+                numberOfInterations++;
+                //// Console.Write (numberOfInterations + ", ");
+            }
+            Console.WriteLine (hash + " input Data " + rawData);
+        }
+
+        public static void GenerateHashcodeWithDifficultyLevel () {
+            int difficultyLevel = 1;
+            int maxdifficultyLevel = 5;
+
+            for (int i = difficultyLevel; i <= maxdifficultyLevel; i++) {
+                Stopwatch stopwatch = new Stopwatch ();
+                stopwatch.Start ();
+                GenerateHashcodeWithDifficultyLevel (i);
+                stopwatch.Stop ();
+                Console.WriteLine ("Observe the Hash code when the Difficulty Level is " + i.ToString ());
+                Console.WriteLine ("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
+            }
+        }
+
+        static void Main (string[] args) {
+            /*
+            Console.WriteLine ("Observe the Hash code DID NOT change as the input data is same");
+            GenerateHashcodeWithoutDataChange ();
+            Console.WriteLine ("Observe the Hash code changes as the input data changes");
+            GenerateHashcodeWithDataChange ();
+            */
+            GenerateHashcodeWithDifficultyLevel ();
         }
     }
 }
